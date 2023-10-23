b4578b1fc2d98fe2b5a490017cd742fac8536cf4
Christopher <christopherlu@Christophers-MacBook-Pro.local>
2023-10-23 16:14:05 -0400

    working cards with album art, album name, and artisit name, along with functioning button to redirect to write review

--- client/Components/postCreator.jsx
+++ client/Components/postCreator.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Button, Container, Card} from 'react-bootstrap';
 import { Link } from 'react-router-dom';
-import { albumArt } from '../Containers/Search'
+
 
 const postCreator = () => {
 
--- client/Containers/Search.jsx
+++ client/Containers/Search.jsx
@@ -12,10 +12,11 @@ import { useState } from 'react';
 import axios from 'axios';
 import SpotifyWebApi from 'spotify-web-api-node';
 import { useEffect } from 'react';
+import { Link } from 'react-router-dom';
 
 const spotifyApi = new SpotifyWebApi({
   clientId: '638fa075b2e7492490a8ab9eb0a6750e',
-  clientSecret: 'c596188e4c994b29a8a30d195108153d'
+  clientSecret: 'c596188e4c994b29a8a30d195108153d',
 });
 
 const SearchAlbum = ({ accessToken }) => {
@@ -30,8 +31,13 @@ const SearchAlbum = ({ accessToken }) => {
   useEffect(() => {
     const albumCards = albums.map(element => {
       return (
-        <Card>
+        <Card className="text-center">
           <Card.Img src={element.images[0].url} />
+          <Card.Body>
+                <Card.Title>{`${element.name}`}</Card.Title>
+                <Card.Subtitle className="mb-2 text-muted">{`${element.artists[0].name}`}</Card.Subtitle>
+                <Link to='/create-post'><Button className="postReviewButton" varient="primary">Leave Review</Button></Link>
+            </Card.Body>
         </Card>
       );
     });
