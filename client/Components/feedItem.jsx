35814ae3b3efef54be92e643786914005ed96c6b
zackweiss <zweiss1881@gmail.com>
2023-10-23 17:02:28 -0400

    working server

--- client/Components/feedItem.jsx
+++ client/Components/feedItem.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 
 const feedItem = () => {
-/*
+  /*
 album art and written review. 
 */
   return (
     <div>
-      <img src='https://cdn1.iconfinder.com/data/icons/music-and-media-player-ui-glyph-s94/96/Music_Icon_Pack_-_Glyph_vinyl-512.png'/>
-      <p className="caption">worst album ever.</p>
+      <img src='https://cdn1.iconfinder.com/data/icons/music-and-media-player-ui-glyph-s94/96/Music_Icon_Pack_-_Glyph_vinyl-512.png' />
+      <p className='caption'>worst album ever.</p>
     </div>
-  )
-}
+  );
+};
 
 export default feedItem;
